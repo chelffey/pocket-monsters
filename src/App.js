c050a5df1f1ad396494bfe01257501ce28d7bb04
Chelsea Chaffey <chaffeychelsea@gmail.com>
2023-01-19 20:40:47 +1100

    next fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import "./App.css";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>Hi there!</h1>
-      <Home />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import "./pages.css";
 
 export default class Home extends React.Component {
   render() {
+    function getInputPokemon(input) {
+      console.log(input.target.value);
+    }
+
     return (
-      <div>
-        <h1>Pocket Monsters</h1>
-        <p>A pocket full of Pokemon...</p>
-        <h3>Search for a pokemon</h3>
-        <input
-          type="text"
-          name="Pokemon"
-          value={this.state.searchPokemon}
-          onChange={this.handleChange}
-        />
+      <div className="pageContainer">
+        <div className="section">
+          <h1>Pocket Monsters</h1>
+          <p>A pocket full of Pokemon...</p>
+        </div>
+        <div className="section">
+          <h3>Search for a pokemon</h3>
+          <input type="text" onChange={getInputPokemon} />
+        </div>
       </div>
     );
   }
