c33daa79d3fea3bb2c155c10190648674484ef2d
Chelsea Chaffey <chaffeychelsea@gmail.com>
2023-01-19 21:31:06 +1100

    updated states for input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,38 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./pages.css";
+import PokemonBox from "../components/PokemonBox";
 
-export default class Home extends React.Component {
-  render() {
-    function getInputPokemon(input) {
-      console.log(input.target.value);
-    }
+function Home() {
+  const [inputPokemon, setInputPokemon] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(null);
 
-    return (
-      <div className="pageContainer">
-        <div className="section">
-          <h1>Pocket Monsters</h1>
-          <p>A pocket full of Pokemon...</p>
-        </div>
-        <div className="section">
-          <h3>Search for a pokemon</h3>
-          <input type="text" onChange={getInputPokemon} />
-        </div>
-      </div>
-    );
+  function getInputPokemon(input) {
+    setInputPokemon(input.target.value);
+    console.log(input.target.value);
   }
+
+  return (
+    <div className="pageContainer">
+      <div className="section">
+        <h1>Pocket Monsters</h1>
+        <p>A pocket full of Pokemon...</p>
+      </div>
+      <div className="section">
+        <h3>Search for a pokemon</h3>
+        <input type="text" onChange={getInputPokemon} />
+        <button
+          onClick={() => {
+            setSearchTerm(inputPokemon);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="section">
+        {searchTerm && <PokemonBox name={searchTerm} type="fire" />}
+      </div>
+    </div>
+  );
 }
+
+export default Home;
