819fe6dfd91ed78f3f4af52061a1224cd930840c
Chelsea Chaffey <chaffeychelsea@gmail.com>
2023-01-22 22:55:57 +1100

    beginning a pokemon party

--- src/pages/Party.js
+++ src/pages/Party.js
@@ -1,1 +1,19 @@
-export default function Party() {}
+import "./pages.css";
+import "./Party.css";
+import { useState } from "react";
+
+export default function Party() {
+  const [party, setParty] = useState([null, null, null, null, null, null]);
+
+  return (
+    <div class="pageContainer">
+      <h1>Party</h1>
+      <p>Create your own Pokemon party!</p>
+      <div>
+        {party.map((pokemon) => {
+          return <div class="pokemon">{pokemon ? pokemon : "empty"}</div>;
+        })}
+      </div>
+    </div>
+  );
+}
