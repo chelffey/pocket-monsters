dbd34d70457203b0cd1d69cae5ee1b4e74121233
Chelsea Chaffey <chaffeychelsea@gmail.com>
2023-01-24 23:12:02 +1100

    moveset back in card

--- src/components/PokemonBox.js
+++ src/components/PokemonBox.js
@@ -14,47 +14,49 @@ export default function PokemonBox(props) {
   return (
     <div className="box">
       <div className="card">
-        <h1>{props.info.name}</h1>
-        <img
-          src={`${props.info.sprites.back_shiny}`}
-          alt={props.info.name + " sprite"}
-        />
-        <img
-          src={`${props.info.sprites.front_default}`}
-          alt={props.info.name + " shiny sprite"}
-        />
+        <div className="padding">
+          <h1>{props.info.name}</h1>
+          <img
+            src={`${props.info.sprites.back_shiny}`}
+            alt={props.info.name + " sprite"}
+          />
+          <img
+            src={`${props.info.sprites.front_default}`}
+            alt={props.info.name + " shiny sprite"}
+          />
 
-        <p>
-          type:
-          {props.info.types.map((a) => {
-            return <span> {a.type.name}</span>;
-          })}
-        </p>
-        <p>Abilities:</p>
-        <ul>
-          {props.info.abilities.map((a) => {
-            return (
-              <li>
-                {a.ability.name} {a.is_hidden && " (hidden)"}
-              </li>
-            );
-          })}
-        </ul>
-        <p>height: {props.info.height} decimetres</p>
-        <p>base experience: {props.info.base_experience} exp</p>
-        <p>id: {props.info.id}</p>
-      </div>
+          <p>
+            type:
+            {props.info.types.map((a) => {
+              return <span> {a.type.name}</span>;
+            })}
+          </p>
+          <p>Abilities:</p>
+          <ul>
+            {props.info.abilities.map((a) => {
+              return (
+                <li>
+                  {a.ability.name} {a.is_hidden && " (hidden)"}
+                </li>
+              );
+            })}
+          </ul>
+          <p>height: {props.info.height} decimetres</p>
+          <p>base experience: {props.info.base_experience} exp</p>
+          <p>id: {props.info.id}</p>
 
-      <button onClick={handleShowMoves}>
-        {showMoves ? "Hide Moveset" : "Show Moveset"}
-      </button>
-      {showMoves && (
-        <div className="movesListGrid">
-          {props.info.moves.map((move) => {
-            return <div className="move">{move.move.name}</div>;
-          })}
+          <button onClick={handleShowMoves}>
+            {showMoves ? "Hide Moveset" : "Show Moveset"}
+          </button>
         </div>
-      )}
+        {showMoves && (
+          <div className="movesListGrid">
+            {props.info.moves.map((move) => {
+              return <div className="move">{move.move.name}</div>;
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
